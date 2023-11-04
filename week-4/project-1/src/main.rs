f5b1a7c77d38bfa3d22d978c991438fc0802c7d3
Abbah Emmanuel <emmanuel.abbah@pau.edu.ng>
2023-11-04 17:52:10 +0100

    (week-4)

--- week-4/project-1/src/main.rs
+++ week-4/project-1/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    let dk1:f64 = 80.0;
+    let t1:f64 = 2.0;
+    let c:f64 = 1.609;
+    let dm1 = dk1 * c;
+
+    //speed of the journey
+    let s1 = dm1 / t1;
+    println!("The speed is:{}", s1);
+    
+    //Another Question
+    let dk2:f64 = 120.0;
+    let t2:f64 = 4.0;
+    let c:f64 = 1.609;
+    let dm2 = dk2 * c;
+    let s2 = dm2 / t2;
+    println!("The speed is: {}", s2);
+
 }
