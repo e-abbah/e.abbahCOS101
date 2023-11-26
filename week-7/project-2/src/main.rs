20295d1ac0f1074fc5dace5527b7e1e145652fc1
e.abbahCOS101 <emmanuel.abbah@pau.edu.ng>
2023-11-26 17:34:48 +0100

    week-7

--- week-7/project-2/src/main.rs
+++ week-7/project-2/src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     let n:u32 = input1.trim().parse().expect("not a valid nuber");
 
     for _i in 1..n+1{
-        println!("Enter your first name of each siblings");
+        println!("Enter the first name of each siblings");
         let mut input2 = String::new();
         io::stdin().read_line(&mut input2).expect("not a valid number");
         let _first_name = input2.trim();
@@ -42,6 +42,10 @@ fn main() {
                     let mut input9 = String::new();
                     io::stdin().read_line(&mut input9).expect("not a valid string");
                     let _course_of_study  = input9.trim();
+                    
+                    println!("The details of your sibling is as follows: ");
+                    println!("First name: {} \n Age: {} \n Single: {} \n Student: {} \n Course of study: {}",_first_name, _age, _single, _student, _course_of_study );
+
 
 
                 }
@@ -56,6 +60,9 @@ fn main() {
                 let mut input11 = String::new();
                 io::stdin().read_line(&mut input11).expect("not a valid string");
                 let _city = input11.trim();
+                
+                println!("The details of sibling is as follows: ");
+                println!("First name: {} \n Age: {} \n Single: {} \n City of residence: {} ", _first_name, _age, _single, _city);
 
 
 
@@ -70,15 +77,20 @@ fn main() {
                 let mut input13 = String::new();
                 io::stdin().read_line(&mut input13).expect("not a valid string");
                 let _sec_school = input13.trim();
+
+                println!("The detail of sibling is as follows: ");
+                println!("First name: {} \n Age: {} \n Do you have waec result: {} \n Secondary school attended: {}", _first_name, _age, _waec_status, _sec_school);
             } 
         } else {
             println!("What class are you in currently:");
             let mut input14 = String::new();
             io::stdin().read_line(&mut input14).expect("not a valid string");
             let _current_class = input14.trim();
+
+            println!("The details of siblings is as follows: ");
+            println!("First name: {} \n Age: {} \n Current class in: {}", _first_name, _age, _current_class);
         }
-        println!("The name of your sibling is {}", _first_name);
-        println!("The age of your sibling is {}", _age)
+        
 
         
     
