09e293d0f9b2d84de87a1cc4b3b2efea9615c537
Abbah Emmanuel <emmanuel.abbah@pau.edu.ng>
2023-12-09 19:53:59 +0100

    week-9

---
+++ week-9/practice-1/target/debug/deps/practice_1.d
@@ -0,0 +1,5 @@
+C:\Users\HP\Documents\e.abbahCOS101\week-9\practice-1\target\debug\deps\practice_1.exe: src\main.rs
+
+C:\Users\HP\Documents\e.abbahCOS101\week-9\practice-1\target\debug\deps\practice_1.d: src\main.rs
+
+src\main.rs:
---
+++ week-9/project-2/target/debug/project-2.d
@@ -0,0 +1,1 @@
+C:\Users\HP\Documents\e.abbahCOS101\week-9\project-2\target\debug\project-2.exe: C:\Users\HP\Documents\e.abbahCOS101\week-9\project-2\src\main.rs
